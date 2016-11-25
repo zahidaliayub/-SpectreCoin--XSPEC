69c199f4f54f29e004095ae034338c19474d9187
Heidi Kaulitz <spectrecoin@users.noreply.github.com>
2016-11-25 08:29:24 +0000

    Update eckey.h

--- src/eckey.h
+++ src/eckey.h
@@ -1,6 +1,7 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2013 The Bitcoin developers
-// Copyright (c) 2014-2015 The SpectreCoin developers
+// Copyright (c) 2014 The ShadowCoin developers
+// Copyright (c) 2016 The SpectreCoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file license.txt or http://www.opensource.org/licenses/mit-license.php.
 
