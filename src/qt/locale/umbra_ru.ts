f7fd4e76ace544ad7faca8fc61e9f25ce5ad49df
Heidi Kaulitz <spectrecoin@users.noreply.github.com>
2016-12-01 20:40:22 +0000

    Update umbra_ru.ts

--- src/qt/locale/umbra_ru.ts
+++ src/qt/locale/umbra_ru.ts
@@ -16,10 +16,12 @@
         <source>Copyright © 2009-2014 The Bitcoin developers
 Copyright © 2012-2014 The NovaCoin developers
 Copyright © 2014 The BlackCoin developers
+Copyright © 2014 The ShadowCoin developers
 Copyright © 2016 The Spectre developers</source>
         <translation>Все права защищены © 2009-2014 Разработчики Bitcoin
 Все права защищены © 2012-2014 Разработчики NovaCoin
-Все права защищены © 2014-2015 Разработчики SpectreCoin</translation>
+Все права защищены © 2014-2015 Разработчики ShadowCoin
+Все права защищены © 2016 Разработчики SpectreCoin</translation>
     </message>
     <message>
         <location line="+16"/>
