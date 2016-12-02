2b55b40bb6459a8a0b5557ebe58ba10bc216e44c
Heidi Kaulitz <spectrecoin@users.noreply.github.com>
2016-12-02 07:12:44 +0000

    Update mnemonic_tests.cpp

--- src/test/mnemonic_tests.cpp
+++ src/test/mnemonic_tests.cpp
@@ -1,4 +1,5 @@
-// Copyright (c) 2015 The SpectreCoin developers
+// Copyright (c) 2014 The ShadowCoin developers
+// Copyright (c) 2016 The SpectreCoin developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
